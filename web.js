eb4d4a88b5437b551543f7fb6b58e4d3a304ce94
Giovani Francischelli <gifrancischelli@gmail.com>
2016-11-17 12:11:59 -0200

    Initialize Keystone

---
+++ web.js
@@ -0,0 +1,29 @@
+const keystone = require('keystone');
+
+keystone.init({
+
+    'name': 'webdoc',
+
+    'favicon': 'public/favicon.ico', 
+    'sass': 'public', 
+    'static': ['public'], 
+
+    'views': 'templates/views',
+    'view engine': 'jade',
+
+    'auto update': 'jade', 
+    'mongo': 'mongodb://localhost/webdoc', 
+
+    'session': true,
+    'auth': true, 
+    'user model': 'User', 
+    'cookie secret': "52M336H&7wK9ae%Q0`ID01pTn6@CpD", 
+
+});
+
+
+require('./models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
