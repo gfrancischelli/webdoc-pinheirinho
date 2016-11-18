f63eb85ea4f7af76b474f56911eb2853d4f61775
Giovani Francischelli <gifrancischelli@gmail.com>
2016-11-18 14:28:31 -0200

    Add basic css structure

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,8 +1,13 @@
 const keystone = require('keystone');
 
+
 exports = module.exports = function(req, res) {
 
+    const locals = res.locals;
+
     const view = new keystone.View(req, res);
+
+    locals.title = "Webdoc Pinheirinho";
 
     view.render('index');
 
