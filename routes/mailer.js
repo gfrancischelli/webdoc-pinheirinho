6c6d71ad968d1b14c27ee3167a6a2f5e1a5d6c07
Giovani Francischelli <gifrancischelli@gmail.com>
2016-12-02 23:43:46 -0200

    Dumber than before, Mail working.

--- routes/mailer.js
+++ routes/mailer.js
@@ -3,10 +3,7 @@ const Mailgun = require('mailgun-js');
 
 exports = module.exports = function(req, res) {
 
-    const mailgun = new Mailgun({
-        apiKey: "key-851e14dc7571b7b1568f8c054a73fd59", 
-        domain: "https://api.mailgun.net/v3/sandbox15deb2d8f5b14ad293686e5c8d9a88f7.mailgun.org/messages", 
-    });
+    const mailgun = new Mailgun(secrets.mailgun_auth);
 
     const sender_name = req.body.FullName;
     const sender_address = req.body.Email;
@@ -14,10 +11,10 @@ exports = module.exports = function(req, res) {
     const mail_content = req.body.Content;
 
     const data = {
-        from: 'Mailgun Sandbox <postmaster@sandbox15deb2d8f5b14ad293686e5c8d9a88f7.mailgun.org>',
-        to: 'Giovanni <gifrancischelli@gmail.com>',
-        subject: "Funcitona em nome de jesus",
-        text: "funciona em nome de jesus",
+        from: 'Webdoc Pinheirinho <postmaster@sandbox15deb2d8f5b14ad293686e5c8d9a88f7.mailgun.org>',
+        to: secrets.mail_to,
+        subject: mail_subject,
+        text: mail_content,
     };
 
     mailgun.messages().send(data, function(error, body) {
