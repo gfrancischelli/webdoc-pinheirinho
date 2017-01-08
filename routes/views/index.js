9d59fe47e3a59c7861832d1498fc0785e56a382e
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-01-08 17:42:51 -0200

    Add APP_DATA global obj

--- routes/views/index.js
+++ routes/views/index.js
@@ -3,16 +3,20 @@ const keystone = require('keystone'),
       Gallery = keystone.list('Gallery'),
       sanitizeHtml = require('sanitize-html');
 
-function queryRecent(model, limit = 3) {
+function queryRecent(model, config = {}) {
+    const where = config.where || {p: 'status', v: 'publicado'};
+    const sort = config.sort || '-publishedAt';
+    const limit = config.limit || 3;
+
     const q = model.find()
         .limit(limit)
         .sort('-publishedAt')
 
-    if (model.schema.paths.status) q.where('status', 'publicado');
+    if (model.schema.paths.status) q.where(where.p, where.v);
 
     return new Promise(function(resolve, reject) {
-        q.sort('-publishedAt')
-            .exec(function(err, results) {
+        q.sort(sort)
+          .exec(function(err, results) {
                 if (results != null) resolve(results);
                 reject(err);
             });
@@ -26,13 +30,24 @@ exports = module.exports = function(req, res) {
 
   locals.data = {};
   locals.title = "home";
+
+  const timeline_config = {
+    where: {p: 'tipo', v: 'timeline'},
+    sort: 'data',
+  }
     
   view.on('init', function(next) {
-    Promise.all([queryRecent(Post.model), queryRecent(Gallery.model)])
-      .then(function([news, galleries]) {
-        news.forEach( post => post.content = sanitizeHtml(post.content) );
+    Promise.all([
+      queryRecent(Post.model),
+      queryRecent(Gallery.model),
+      queryRecent(Post.model, timeline_config)
+    ])
+      .then(function([news, galleries, timeline]) {
+        news.forEach( post => post.content = sanitizeHtml(post.content));
+        timeline.forEach( post => post.content = sanitizeHtml(post.content));
         locals.data.news = news;
         locals.data.galleries = galleries;
+        locals.data.timeline = timeline;
         next();
       });
   });
