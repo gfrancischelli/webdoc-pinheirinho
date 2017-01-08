a4a17520696bf9ffd8b5f7f59c2fb72ae75d8fc8
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-01-08 17:39:18 -0200

    Add Jest

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,16 @@ global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 
+const mockResponse = (status, statusText, response) => {
+  return new window.Response(response, {
+      status: status,
+      statusText: statusText,
+      headers: {
+            'Content-type': 'application/json'
+          }
+    });
+};
+
 console.error = message => {
   if (!/(React.createElement: type should not be null)/.test(message)) {
       throw new Error(message);
