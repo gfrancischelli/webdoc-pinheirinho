9cd4f2ecd77cfeb0e20338934adfdb2d89fc4d8e
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-01-20 18:51:25 -0200

    Add new navbar collapse behaviour

--- client/components/Header/Navbar.js
+++ client/components/Header/Navbar.js
@@ -4,18 +4,14 @@ import { Link } from 'react-router';
 import breakpoints from '../../media-query';
 import measure from 'remeasure';
 
-const links = {
-  always_visible: [
+const links = [
     { name: <span className='fa fa-home' />, path: "/#home" },
     { name: "galerias", path: "/galerias" },
     { name: "linha do tempo", path: "/timeline" },
     { name: "notícias", path: "/noticias" },
-  ],
-  mobile_hidden: [
     { name: "ficha técnica", path: "/ficha-tecnica" },
     { name: "fale conosco", path: "#fale-conosco" },
-  ],
-}
+]
 
 class Nav extends React.Component {
   constructor() {
@@ -27,11 +23,52 @@ class Nav extends React.Component {
   }
 
   componentDidMount() {
+    const dragHandler = this.dragHandler;
+    const viewportWidth = document.body.clientWidth;
+    const menuWidth = this.Menu.scrollWidth;
+
     document.addEventListener('scroll', this.updatePosition)
+
+    if (viewportWidth < menuWidth) {
+
+      this.Nav.addEventListener('mousedown', (e) => {
+        this.mouseX = e.x;
+        this.menuOffsetX = this.Menu.getBoundingClientRect().left;
+
+        this.Nav.addEventListener('mousemove', dragHandler, true);
+      });
+
+      this.Nav.addEventListener('mouseup', (e) => {
+        this.Nav.removeEventListener('mousemove', dragHandler, true)
+      });
+
+      this.Nav.addEventListener('mouseout', () => {
+        this.Nav.removeEventListener('mousemove', dragHandler, true)
+      });
+
+      this.Nav.addEventListener('mouseleave', () => {
+        this.Nav.removeEventListener('mousemove', dragHandler, true)
+      });
+
+    }
+  }
+
+  dragHandler = (e) => {
+    const viewportWidth = document.body.clientWidth;
+    const { left } = this.Menu.getBoundingClientRect();
+    const right = this.Menu.scrollWidth - Math.abs(left);
+
+    const newMouseX = e.x;
+    const deltaX = newMouseX - this.mouseX + this.menuOffsetX;
+
+    if ( left < 0 && right < viewportWidth && newMouseX < this.mouseX ) return 0;
+    if ( left > 0 && right > viewportWidth && newMouseX > this.mouseX ) return 0;
+
+    this.Menu.style.transform = `translateX(${ deltaX }px)`;
   }
 
   updatePosition = () => {
-    // initialY is a hack to correctly set position
+    // initialy is a hack to correctly set position
     // don't know why the position is wrong when
     // calculating on mount
     const top = this.Nav.offsetTop
@@ -65,30 +102,16 @@ class Nav extends React.Component {
     </li>
   )
 
-  toggle = () => {
-    this.setState( (prevState, props) => {
-      return {open: !prevState.open}
-    });
-  }
   render() {
     const onHeaderClass = this.state.isOnHeader ? 'is-on-header' : '';
     return (
       <nav
         ref={ c => this.Nav = c }
         className={`c-site-nav ${onHeaderClass}`}>
-        <ul className='c-site-nav__list'>
-          { links.always_visible.map( this.renderLink )}
-          <li className='c-site-nav__item'>
-            <button 
-              onClick={ this.toggle }>
-              <span className='fa fa-bars' />
-            </button>
-          </li>
-          { !this.state.open ? null :
-            <div className='dropdown'>
-              {links.mobile_hidden.map( this.renderLink )}
-            </div>
-          }
+        <ul
+          ref={ c => this.Menu = c }
+          className='c-site-nav__list'>
+          { links.map( this.renderLink )}
         </ul>
         <style jsx>{`
           .dropdown {
