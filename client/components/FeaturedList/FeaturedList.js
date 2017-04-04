820467c4e3f37a3a3c11852471bacdf83e297dde
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-04-04 18:50:47 -0300

    Fix formating

--- client/components/FeaturedList/FeaturedList.js
+++ client/components/FeaturedList/FeaturedList.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router';
-import ImageSet from 'components/ImageSet/ImageSet';
-import DangerousHTML from 'components/DangerousHTML/DangerousHTML';
+import React from "react";
+import { Link } from "react-router";
+import ImageSet from "components/ImageSet/ImageSet";
+import DangerousHTML from "components/DangerousHTML/DangerousHTML";
 
-import {imageURL, resourceURL} from 'utils';
+import { imageURL, resourceURL } from "utils";
 
 function FeaturedList({ featured, type }) {
   return (
-    <ul className='o-feat-list'>
-      { featured.map( (item, index) => (
-        <li key={ index } className='o-feat-list__item o-card'>
-          { !item.cover ? null :
-            <ImageSet 
-              src={ imageURL(type, item.cover.filename)}
-              className='o-card__figure'
-            />
-          }
-          <div className='o-card__body'>
-            <Link to={ resourceURL(type, item.slug) }>
-              <h3 className='c-heading c-heading--small'>
-                { item.title }
+    <ul className="o-feat-list">
+      {featured.map((item, index) => (
+        <li key={index} className="o-feat-list__item o-card">
+          {!item.cover
+            ? null
+            : <ImageSet
+                src={imageURL(type, item.cover.filename)}
+                className="o-card__figure"
+              />}
+          <div className="o-card__body">
+            <Link to={resourceURL(type, item.slug)}>
+              <h3 className="c-heading c-heading--small">
+                {item.title}
               </h3>
             </Link>
-            { !item.content ? null :
-              <DangerousHTML content={item.content.slice(0, 200) + ' ...'} />
-            }
+            {!item.content
+              ? null
+              : <DangerousHTML content={item.content.slice(0, 200) + " ..."} />}
           </div>
         </li>
       ))}
     </ul>
-  )  
+  );
 }
 
 FeaturedList.propTypes = {
   featured: React.PropTypes.array
-}
+};
 
 export default FeaturedList;
